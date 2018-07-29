9f9cfdfcfb35e5b6a36dcd4f9cb35da254d1c319
Minh Le <minhle.r7@gmail.com>
2018-07-29 22:49:04 +0200

    fix bug

--- cort/core/mention_property_computer.py
+++ cort/core/mention_property_computer.py
@@ -236,7 +236,7 @@ def compute_head_information(attributes):
     if len(mention_subtree.leaves()) == len(attributes["tokens"]):
         head_tree = head_finder.get_head(mention_subtree)
         head_index = get_head_index(head_tree, mention_subtree.pos())
-        if isinstance(head_tree[0]):
+        if isinstance(head_tree[0], str):
             head = [head_tree[0]]
         else:
             head = head_tree.leaves()
