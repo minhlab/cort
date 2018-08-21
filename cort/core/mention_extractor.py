931bec3d88aa92d198e2a0a2c2ab316efdf59b58
Minh Le <minhle.r7@gmail.com>
2018-08-21 21:39:25 +0200

    use gold mention spans always

--- cort/core/mention_extractor.py
+++ cort/core/mention_extractor.py
@@ -11,138 +11,16 @@ __author__ = 'smartschat'
 
 
 def extract_system_mentions(document, filter_mentions=True):
-    """ Extract mentions from parse trees and named entity layers in a document.
-
-    Args:
-        document (ConLLDocument): The document from which mentions should be
-            extracted.
-        filter_mentions (bool): Indicates whether extracted mentions should
-            be filtered. If set to True, filters:
-
-                - mentions with the same head (retains one with largest span),
-                - mentions whose head is embedded in another mention's head,
-                - mentions whose head as POS tag JJ,
-                - mentions of namend entity type QUANTITY, CARDINAL, ORDINAL,
-                  MONEY or PERCENT,
-                - mentions "mm", "hmm", "ahem", "um", "US" and "U.S.",
-                - non-pronominal mentions embedded in appositions, and
-                - pleonastic "it" and "you" detected via heuristics
-
+    """ 
     Returns:
         list(Mention): the sorted list of extracted system mentions. Includes a
         "dummy mention".
     """
-    system_mentions = [mentions.Mention.from_document(span, document)
-                       for span in __extract_system_mention_spans(document)]
-
-    if filter_mentions:
-        for post_processor in [
-            post_process_same_head_largest_span,
-            post_process_embedded_head_largest_span,
-            post_process_by_head_pos,
-            post_process_by_nam_type,
-            post_process_weird,
-            post_process_appositions,
-            post_process_pleonastic_pronoun
-        ]:
-            system_mentions = post_processor(system_mentions)
-
-    seen = set()
-
-    # update set id and whether it is the first mention in gold entity
-    for mention in system_mentions:
-        mention.attributes["set_id"] = None
-
-        annotated_set_id = mention.attributes["annotated_set_id"]
-
-        mention.attributes["first_in_gold_entity"] = \
-            annotated_set_id not in seen
-
-        seen.add(annotated_set_id)
-
+    system_mentions = [mentions.Mention.from_document(m.span, document)
+                       for m in document.annotated_mentions]
     system_mentions = [mentions.Mention.dummy_from_document(document)] \
         + system_mentions
-
     return system_mentions
-
-
-def __extract_system_mention_spans(document):
-    mention_spans = []
-    for i, sentence_span in enumerate(document.sentence_spans):
-        sentence_tree = document.parse[i]
-
-        in_sentence_spans = __extract_mention_spans_for_sentence(
-            sentence_tree,
-            document.ner[sentence_span.begin:sentence_span.end+1])
-
-        mention_spans += [spans.Span(sentence_span.begin + span.begin,
-                                     sentence_span.begin + span.end)
-                          for span in in_sentence_spans]
-
-    return sorted(mention_spans)
-
-
-def __extract_mention_spans_for_sentence(sentence_tree, sentence_ner):
-    return sorted(list(set(
-        [__get_in_tree_span(subtree) for subtree
-            in sentence_tree.subtrees(__tree_filter)]
-        + __get_span_from_ner(
-            [pos[1] for pos in sentence_tree.pos()], sentence_ner)
-    )))
-
-
-def __extract_mention_spans_from_tree(sentence_tree):
-    mention_spans = []
-
-    for subtree in sentence_tree.subtrees(__tree_filter):
-        mention_spans.append(__get_in_tree_span(subtree))
-
-    return sorted(mention_spans)
-
-
-def __tree_filter(tree):
-    return tree.label() == "NP" or tree.label() == "PRP$"
-
-
-def __get_span_from_ner(pos, ner):
-    i = 0
-    spans_from_ner = []
-    while i < len(ner):
-        current_tag = ner[i]
-        if current_tag != "NONE":
-            start = i
-
-            while i+1 < len(ner) and ner[i+1] != "NONE" and ner[i] == ner[i+1]:
-                i += 1
-
-            if i+1 < len(pos) and pos[i+1] == "POS":
-                i += 1
-
-            spans_from_ner.append(spans.Span(start, i))
-
-        i += 1
-
-    return sorted(spans_from_ner)
-
-
-def __get_in_tree_span(parented_tree):
-    start = 0
-
-    current_tree = parented_tree
-
-    while current_tree.parent() is not None:
-        for child in current_tree.parent():
-            if child == current_tree:
-                break
-            else:
-                start += len(child.leaves())
-
-        current_tree = current_tree.parent()
-
-    end = start + len(parented_tree.leaves()) - 1
-
-    return spans.Span(start, end)
-
 
 def post_process_by_head_pos(system_mentions):
     """ Removes mentions whose head has the part-of-speech tag JJ.
